71c841fac243e1bee23581d1a7059660d33cf1d4
Zac Messinger <zac.messinger@gmail.com>
2018-10-21 11:58:32 -0700

    Set up minimum config for apollo client, and added apollo boost

--- client/index.js
+++ client/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost'; // Library for interacting with GraphQL server on the backend, i.e making requests for data and storing locally.
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <div>Lyrical</div>
+    </ApolloProvider>
+  )
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />,document.querySelector('#root'));
